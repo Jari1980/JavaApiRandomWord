235c4c7de1c67ed38f9d6e015ebe628045a21b43
Jari Tenhunen <jari.tenhunen@outlook.com>
2024-10-07 16:17:54 +0200

    Initial commit

---
+++ src/main/java/com/Jari/API_RandomWord/ApiRandomWordApplication.java
@@ -0,0 +1,13 @@
+package com.Jari.API_RandomWord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRandomWordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRandomWordApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Jari/API_RandomWord/ApiRandomWordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Jari.API_RandomWord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRandomWordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
