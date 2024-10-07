eb1ceda9255e4554c6983afaabba98dc3fbbb286
Jari Tenhunen <jari.tenhunen@outlook.com>
2024-10-07 16:39:52 +0200

    Think this will do, next creating a prepolated list of words in controller

---
+++ src/main/java/com/Jari/API_RandomWord/Word.java
@@ -0,0 +1,18 @@
+package com.Jari.API_RandomWord;
+
+import com.fasterxml.jackson.annotation.JsonTypeId;
+
+import javax.annotation.processing.Generated;
+import java.util.ArrayList;
+
+public class Word {
+    String word;
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+}
